12eed33713bb9c49b96f6c49b8c5def3ec49a220
MUTHUAKALYA <muthuakalyaa@gmail.com>
2023-10-13 04:30:54 -0700

    design:curd-rice-landing-page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'dark-blue': '#13547a',
+      'light-blue': '#80d0c7',
+      'white':'#FFF',
+      'black':'#000'
+    },
     extend: {},
   },
   plugins: [],
